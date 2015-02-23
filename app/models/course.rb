3ccb41e4a6aa7297490d89c99770f60050c3657e
Audrey Wong <audreydotwong@gmail.com>
2015-02-23 22:59:22 +0000

    built sections

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,5 +1,6 @@
 class Course < ActiveRecord::Base
 	belongs_to :user
+	has_many :sections
 
 	validates :title, :presence => { :message => "Doh, you didn't enter a title!" }
 	validates :description, :presence => true
