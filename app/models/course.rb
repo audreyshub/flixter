1a51b49b94cd94b5106a64ae84e0bc2b1d322c1b
Audrey Wong <audreydotwong@gmail.com>
2015-02-23 20:41:02 +0000

    list out courses

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,7 +1,7 @@
 class Course < ActiveRecord::Base
 	belongs_to :user
 
-	validates :title, :presence => true
+	validates :title, :presence => { :message => "Doh, you didn't enter a title!" }
 	validates :description, :presence => true
 	validates :cost, :presence => true, :numericality => {:greater_than_or_equal_to => 0}
 end
